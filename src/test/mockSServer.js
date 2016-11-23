ecddd66a628a0ed21865e7c57edde06c75752ecb
fablec <fabrice.lecoz@telecomsante.com>
2016-11-23 18:03:48 +0100

    move sources to src

--- src/test/mockSServer.js
+++ src/test/mockSServer.js
@@ -1,3 +1,25 @@
+/**
+ @license
+ Copyright (c) 2016 Telecom Sante
+ This code may only be used under the CC BY-NC 4.0 style license found at https://creativecommons.org/licenses/by-nc/4.0/legalcode
+
+ You are free to:
+
+ Share — copy and redistribute the material in any medium or format
+ Adapt — remix, transform, and build upon the material
+ The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+ Under the following terms:
+
+ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+ NonCommercial — You may not use the material for commercial purposes.
+
+ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others
+ from doing anything the license permits.
+ */
+
 /* jslint node:true */
 
 "use strict";
@@ -8,18 +30,6 @@ var restify = require("restify"),
 	Logger  = require("logger");
 
 var logger = new Logger( "Mock SServer");
-
-// for https server
-/*
-var server = restify.createServer({
-	name:    "SServer mock",
-	version: "0.0.1",
-	certificate: fs.readFileSync("./test/server.crt"),
-    key : fs.readFileSync("./test/server.key"),
-    requestCert: true,
-    rejectUnauthorized:false
-});
-*/
 
 var pkg     = require('../package.json');
 var config = {};
